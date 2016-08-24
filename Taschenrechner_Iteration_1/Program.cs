28cb0038a3984027a49308ec07cc25fc8e0e8cba
minkerda <minkerda@LDE8367>
2016-08-24 07:44:42 +0200

    Implementiert: - User Abfrage von zwei Zahlen des Typs [Int32] - Berechnung und Ausgabe des Summanden [Int64] - Error Handler (Format,OverFlow) - Alle Funktionen getestet
    Offen:
    - Abbruch der Anwendung durch Tasteneingabe "Q","q"

---
+++ Taschenrechner_Iteration_1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taschenrechner_Iteration_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Addieren: Als Benutzer möchte ich zwei Zahlen eingeben und deren Summanden berechnen lassen.
+            //Beenden:  Als Benutzer möchte ich die Anwendung zuverlässig beenden können.
+            //Starten:  Als Benutzer möchte ich, dass das Programm schnell startet damit schnell und effizient arbeiten kann.
+
+            try
+            {
+                Console.Write("Erste Zahl eingeben[Int32]: ");
+                Int32 erste_zahl = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Zweite Zahl eingeben[Int32]: ");
+                Int32 zweite_zahl = Convert.ToInt32(Console.ReadLine());
+
+                Int64 ergebniss = erste_zahl + zweite_zahl;
+                Console.WriteLine("Das Ergbniss aus {0} + {1} = {2}", erste_zahl, zweite_zahl, ergebniss);
+
+                Console.ReadKey();
+            }
+
+            catch (FormatException)
+            {
+                Console.WriteLine("Die Eingabe entspricht nicht einem Int32-Format.");
+                Console.ReadKey();
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Die Eingabe ist größer als der maximale Int32-Wert.");
+                Console.ReadKey();
+            }
+
+        }
+        
+    }
+}
