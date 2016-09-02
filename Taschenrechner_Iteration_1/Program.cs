e4cdd4e3e6ccc14e68244ed8b2aa22d74eca34a0
David <davidminker@gmail.com>
2016-09-02 21:15:06 +0200

    -Addierenfunktion von  Ganzezahlen zu Gleitkommazahlen geändert

--- Taschenrechner_Iteration_1/Program.cs
+++ Taschenrechner_Iteration_1/Program.cs
@@ -1,29 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using System.ComponentModel;
 
 namespace Taschenrechner_Iteration_1
 {
     class Program
     {
+
+        private static BackgroundWorker worker = new BackgroundWorker();
+        
         static void Main(string[] args)
         {
             //Addieren: Als Benutzer möchte ich zwei Zahlen eingeben und deren Summanden berechnen lassen.
             //Beenden:  Als Benutzer möchte ich die Anwendung zuverlässig beenden können.
             //Starten:  Als Benutzer möchte ich, dass das Programm schnell startet damit schnell und effizient arbeiten kann.
 
+            //worker = new BackgroundWorker();
+            /*worker.DoWork += worker_DoWork;
+            worker.WorkerReportsProgress = true;
+            worker.RunWorkerAsync();*/
+
             try
             {
-                Console.Write("Erste Zahl eingeben[Int32]: ");
-                Int32 erste_zahl = Convert.ToInt32(Console.ReadLine());
+                //User Story: Werte addieren
+                Console.Write("Erste Zahl eingeben: ");
+                float erste_zahl = Convert.ToSingle(Console.ReadLine());
 
-                Console.Write("Zweite Zahl eingeben[Int32]: ");
-                Int32 zweite_zahl = Convert.ToInt32(Console.ReadLine());
+                //Wandel von Text zu Gleitkommazahlen
+                Console.Write("Zweite Zahl eingeben: ");
+                float zweite_zahl = Convert.ToSingle(Console.ReadLine());
 
-                Int64 ergebniss = erste_zahl + zweite_zahl;
-                Console.WriteLine("Das Ergbniss aus {0} + {1} = {2}", erste_zahl, zweite_zahl, ergebniss);
+                //Wandel von Text zu Gleitkommazahlen
+                float ergebniss = erste_zahl + zweite_zahl;
+                Console.WriteLine("Das Ergebniss aus {0} + {1} = {2}", erste_zahl, zweite_zahl, ergebniss);
 
                 Console.ReadKey();
             }
@@ -40,6 +49,24 @@ namespace Taschenrechner_Iteration_1
             }
 
         }
-        
+        /// <summary>
+        /// Check for cancelation by the user
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /*static void worker_DoWork (object sender, DoWorkEventArgs e)
+        {
+            if (Console.ReadKey(true).Key == ConsoleKey.Escape)
+            {
+                Console.WriteLine("\n------------------------------------------");
+                Console.WriteLine("Programm wurde durch den Anwender beendet!");
+                Console.WriteLine("Taste drücken um das Programm zu verlassen.");
+                Console.ReadKey();
+                System.Environment.Exit(0);
+            }
+        }*/
+
+
+
     }
 }
