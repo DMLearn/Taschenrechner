1b978bbec9dd5fff53113976ecbed321fa85cdd0
David <davidminker@gmail.com>
2016-09-04 13:49:52 +0200

    AssemblyInfo.cs angepasst

--- Taschenrechner/Properties/AssemblyInfo.cs
+++ Taschenrechner/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden 
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("Taschenrechner_Iteration_1")]
+[assembly: AssemblyTitle("Taschenrechner")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Draeger")]
-[assembly: AssemblyProduct("Taschenrechner_Iteration_1")]
-[assembly: AssemblyCopyright("Copyright © Draeger 2016")]
+[assembly: AssemblyCompany("DMLearn")]
+[assembly: AssemblyProduct("Taschenrechner")]
+[assembly: AssemblyCopyright("Copyright © David")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+ [assembly: AssemblyVersion("1.0.*")]
+//[assembly: AssemblyVersion("1.0.0.0")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
