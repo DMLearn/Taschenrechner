a4ac9de62811381b270a5c77236a769ac579cac2
David <davidminker@gmail.com>
2016-09-14 20:41:57 +0200

    Alle Methoden für die Berechnung in die seperate Klasse RechnerModel verschoben.

--- Taschenrechner/Program.cs
+++ Taschenrechner/Program.cs
@@ -10,35 +10,22 @@ namespace Taschenrechner
         static void Main(string[] args)
         {
             
-            try
-            {
-                //User Story: Werte addieren
-                string ersteZahlAlsString = HoleBenutzerEingabe("Bitte gib die erste Zahl ein: ");
-                string zweiteZahlAlsString = HoleBenutzerEingabe("Bitte gib die zweite Zahl ein: ");
-                string operation = HoleBenutzerEingabe("Bitte gib die Operation ein (+ - * /): ");
+            
+            //User Story: Werte addieren
+            string ersteZahlAlsString = HoleBenutzerEingabe("Bitte gib die erste Zahl ein: ");
+            string zweiteZahlAlsString = HoleBenutzerEingabe("Bitte gib die zweite Zahl ein: ");
+            string operation = HoleBenutzerEingabe("Bitte gib die Operation ein (+ - * /): ");
 
-                //Wandel von Text zu Gleitkommazahlen
-                double ersteZahlAlsDouble = Convert.ToDouble(ersteZahlAlsString);
-                double zweiteZahlAlsDouble = Convert.ToDouble(zweiteZahlAlsString);
+            //Wandel von Text zu Gleitkommazahlen
+            double ersteZahlAlsDouble = Convert.ToDouble(ersteZahlAlsString);
+            double zweiteZahlAlsDouble = Convert.ToDouble(zweiteZahlAlsString);
 
-                double resultat = 0;
-                resultat = Berechnung(ersteZahlAlsDouble, zweiteZahlAlsDouble, operation);
+            //Berechnung ausführen
+            RechnerModel model = new RechnerModel();
+            double resultat = model.Berechne(ersteZahlAlsDouble, zweiteZahlAlsDouble, operation);
 
-                Console.WriteLine("Das Ergebnis aus {0} {3} {1} = {2}", ersteZahlAlsDouble, zweiteZahlAlsDouble, resultat, operation);
-                HoleBenutzerEingabe("-------------------------------------\nZum Beenden bitte eine Taste drücken!");
-
-            }
-
-            catch (FormatException)
-            {
-                Console.WriteLine("Die Eingabe entspricht nicht einem Zahlenformat.");
-                Console.ReadKey();
-            }
-            catch (OverflowException)
-            {
-                Console.WriteLine("Die Eingabe ist größer als der maximale mögliche Zahlenwert.");
-                Console.ReadKey();
-            }
+            Console.WriteLine("Das Ergebnis aus {0} {3} {1} = {2}", ersteZahlAlsDouble, zweiteZahlAlsDouble, resultat, operation);
+            HoleBenutzerEingabe("-------------------------------------\nZum Beenden bitte eine Taste drücken!");
 
         }
 
@@ -54,87 +41,6 @@ namespace Taschenrechner
 
             return input;
         }
-
-        /// <summary>
-        /// Methode ruft die Berchnungsmethode für die jeweilige Operation auf
-        /// </summary>
-        /// <param name="erste_zahl"></param>
-        /// <param name="zweite_zahl"></param>
-        /// <param name="operation"></param>
-        /// <returns></returns>
-        static double Berechnung(double erste_zahl, double zweite_zahl, string operation)
-        {
-            double resultat = 0;
-
-            switch (operation)
-            {
-                case "+":
-                    resultat = AddierenZweierZahlen(erste_zahl, zweite_zahl);
-                    break;
-                case "-":
-                    resultat = SubtrahierenZweierZahlen(erste_zahl, zweite_zahl);
-                    break;
-                case "*":
-                    resultat = MultiplizierenZweierZahlen(erste_zahl, zweite_zahl);
-                    break;
-                case "/":
-                    resultat = DividierenZweierZahlen(erste_zahl, zweite_zahl);
-                    break;
-                default:
-                    resultat = 0;
-                    break;
-            }
-
-            return resultat;
-        }
-
-        /// <summary>
-        /// Methode addiert zwei Zahlen.
-        /// </summary>
-        /// <param name="erste_zahl"></param>
-        /// <param name="zweite_zahl"></param>
-        static double AddierenZweierZahlen(double erste_zahl, double zweite_zahl)
-        {
-            double summe = erste_zahl + zweite_zahl;
-            return summe;
-        }
-
-        /// <summary>
-        /// Methode subtrahiert zwei Zahlen.
-        /// </summary>
-        /// <param name="erste_zahl"></param>
-        /// <param name="zweite_zahl"></param>
-        /// <returns></returns>
-        static double SubtrahierenZweierZahlen(double erste_zahl, double zweite_zahl)
-        {
-            double differenz = erste_zahl - zweite_zahl;
-            return differenz;
-        }
-
-        /// <summary>
-        /// Methode multipliziert zwei Zahlen
-        /// </summary>
-        /// <param name="erste_zahl"></param>
-        /// <param name="zweite_zahl"></param>
-        /// <returns></returns>
-        static double MultiplizierenZweierZahlen(double erste_zahl, double zweite_zahl)
-        {
-            double produkt = erste_zahl * zweite_zahl;
-            return produkt;
-        }
-
-        /// <summary>
-        /// Methode dividiert zwei Zahlen
-        /// </summary>
-        /// <param name="erste_zahl"></param>
-        /// <param name="zweite_zahl"></param>
-        /// <returns></returns>
-        static double DividierenZweierZahlen(double erste_zahl, double zweite_zahl)
-        {
-            double quotient = erste_zahl / zweite_zahl;
-            return quotient;
-        }
-
 
     }
 }
---
+++ Taschenrechner/RechnerModel.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taschenrechner
+{
+    class RechnerModel
+    {
+
+        /// <summary>
+        /// Methode ruft die Berchnungsmethode für die jeweilige Operation auf
+        /// </summary>
+        /// <param name="erste_zahl"></param>
+        /// <param name="zweite_zahl"></param>
+        /// <param name="operation"></param>
+        /// <returns></returns>
+        public double Berechne(double erste_zahl, double zweite_zahl, string operation)
+        {
+            double resultat = 0;
+
+            switch (operation)
+            {
+                case "+":
+                    resultat = AddierenZweierZahlen(erste_zahl, zweite_zahl);
+                    break;
+                case "-":
+                    resultat = SubtrahierenZweierZahlen(erste_zahl, zweite_zahl);
+                    break;
+                case "*":
+                    resultat = MultiplizierenZweierZahlen(erste_zahl, zweite_zahl);
+                    break;
+                case "/":
+                    resultat = DividierenZweierZahlen(erste_zahl, zweite_zahl);
+                    break;
+                default:
+                    resultat = 0;
+                    break;
+            }
+
+            return resultat;
+        }
+
+        /// <summary>
+        /// Methode addiert zwei Zahlen.
+        /// </summary>
+        /// <param name="erste_zahl"></param>
+        /// <param name="zweite_zahl"></param>
+        private double AddierenZweierZahlen(double erste_zahl, double zweite_zahl)
+        {
+            double summe = erste_zahl + zweite_zahl;
+            return summe;
+        }
+
+        /// <summary>
+        /// Methode subtrahiert zwei Zahlen.
+        /// </summary>
+        /// <param name="erste_zahl"></param>
+        /// <param name="zweite_zahl"></param>
+        /// <returns></returns>
+        private double SubtrahierenZweierZahlen(double erste_zahl, double zweite_zahl)
+        {
+            double differenz = erste_zahl - zweite_zahl;
+            return differenz;
+        }
+
+        /// <summary>
+        /// Methode multipliziert zwei Zahlen
+        /// </summary>
+        /// <param name="erste_zahl"></param>
+        /// <param name="zweite_zahl"></param>
+        /// <returns></returns>
+        private double MultiplizierenZweierZahlen(double erste_zahl, double zweite_zahl)
+        {
+            double produkt = erste_zahl * zweite_zahl;
+            return produkt;
+        }
+
+        /// <summary>
+        /// Methode dividiert zwei Zahlen
+        /// </summary>
+        /// <param name="erste_zahl"></param>
+        /// <param name="zweite_zahl"></param>
+        /// <returns></returns>
+        private double DividierenZweierZahlen(double erste_zahl, double zweite_zahl)
+        {
+            double quotient = erste_zahl / zweite_zahl;
+            return quotient;
+        }
+
+    }
+}
