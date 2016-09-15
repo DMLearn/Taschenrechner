add5e576e3f1a19f25fe8cb9401db161b41c3371
David <davidminker@gmail.com>
2016-09-15 08:34:34 +0200

    Anwendungscontroller implementiert

---
+++ Taschenrechner/AnwendungsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taschenrechner
+{
+    class AnwendungsController
+    {
+        private ConsoleView view;
+        private RechnerModel model;
+
+        //Konstruktor
+        public AnwendungsController(ConsoleView view, RechnerModel model)
+        {
+            this.model = model;
+            this.view = view;
+        }
+
+        public void Ausfuehren()
+        {
+            //Abfrage der jeweiligen Zahlen und der Operation durch die Klassen
+            string ersteZahlAlsString = view.HoleZahlVomBeutzer();
+            string operation = view.HoleOperatorVomBeutzer();
+            string zweiteZahlAlsString = view.HoleZahlVomBeutzer();
+
+            //Wandelt den Text zu Gleitkommazahlen
+            double ersteZahlAlsDouble = Convert.ToDouble(ersteZahlAlsString);
+            double zweiteZahlAlsDouble = Convert.ToDouble(zweiteZahlAlsString);
+
+            //Berechnung ausführen
+            model.Berechne(ersteZahlAlsDouble, zweiteZahlAlsDouble, operation);
+
+            view.GibResultatAus(operation);
+            view.WarteAufEndeDurchBenutzer();
+        }
+    }
+}
--- Taschenrechner/Program.cs
+++ Taschenrechner/Program.cs
@@ -12,21 +12,9 @@ namespace Taschenrechner
             //Objekte für die Klassen von view und model angelegt 
             RechnerModel model = new RechnerModel();
             ConsoleView view = new ConsoleView(model);
+            AnwendungsController controller = new AnwendungsController(view, model);
 
-            //Abfrage der jeweiligen Zahlen und der Operation durch die Klassen
-            string ersteZahlAlsString = view.HoleZahlVomBeutzer();
-            string operation = view.HoleOperatorVomBeutzer();
-            string zweiteZahlAlsString = view.HoleZahlVomBeutzer();
-
-            //Wandelt den Text zu Gleitkommazahlen
-            double ersteZahlAlsDouble = Convert.ToDouble(ersteZahlAlsString);
-            double zweiteZahlAlsDouble = Convert.ToDouble(zweiteZahlAlsString);
-
-            //Berechnung ausführen
-            model.Berechne(ersteZahlAlsDouble, zweiteZahlAlsDouble, operation);
-
-            view.GibResultatAus(operation);
-            view.WarteAufEndeDurchBenutzer();
+            controller.Ausfuehren();
 
         }
     }
